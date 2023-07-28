79543e4e3e93aca52a761068c2fe401adafed48b
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2023-07-28 21:50:07 +0600

    powersupply unit fetched by category

--- src/pages/featured/power-supply-unit.js
+++ src/pages/featured/power-supply-unit.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import Card from "@/components/UI/Card";
 
-const PowerSupply = () => {
+
+
+const PowerSupplyUnit = ({ products }) => {
   return (
     <div>
-      <h1>Power</h1>
+      <h1 className="text-3xl text-sky-600 font-bold font-serif mb-8 text-center">
+        Category: Power Supply Unit
+      </h1>
+      <div className="flex items-center justify-center my-8 mb-28">
+        <div className="grid lg:grid-cols-3 grid-cols-1 gap-4">
+          {products.map((product) => (
+            <Card key={product._id} product={product} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default PowerSupply;
+export async function getStaticProps() {
+  try {
+    const res = await fetch("http://localhost:3001/api/pc-components");
+    const allProducts = await res.json();
+
+    const psuProducts = allProducts.filter(
+      (product) => product.category === "Power Supply Unit"
+    );
+
+    return {
+      props: { products: psuProducts },
+    };
+  } catch (error) {
+    return {
+      props: { products: [] },
+    };
+  }
+}
+
+export default PowerSupplyUnit;
