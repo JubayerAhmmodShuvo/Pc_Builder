3d339f97181e6d60a4f8cc77dd9f47501833e2c8
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2023-07-28 21:58:53 +0600

    home page edited

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import Banner from "@/components/UI/Banner";
+import Card from "@/components/UI/Card";
+
 
 export default function Home({ featuredProducts }) {
   return (
@@ -11,42 +13,7 @@ export default function Home({ featuredProducts }) {
       <div className="flex justify-center my-12">
         <div className="grid lg:grid-cols-3 gap-4">
           {featuredProducts.map((product) => (
-            <div
-              key={product._id}
-              className="p-4 bg-teal-50 card w-96 rounded-lg shadow-md text-black"
-            >
-              <img
-                src={product.image}
-                alt={product.name}
-                className="w-full h-96 object-cover rounded-md"
-              />
-
-              <h3 className="text-xl text-black font-bold mt-2">
-                {product.name}
-              </h3>
-
-              <p className="text-lg text-gray-500 mt-1">{product.category}</p>
-
-              <p className="text-lg font-bold mt-1">
-                ${product.price.toFixed(2)}
-              </p>
-
-              <div className="flex justify-between">
-                <p
-                  className={`mt-1 ${
-                    product.status === "In Stock"
-                      ? "text-green-600"
-                      : "text-red-600"
-                  }`}
-                >
-                  {product.status}
-                </p>
-                <div className=" mt-1 ">
-                  <span className="text-yellow-500   mr-1">★</span>
-                  <span className="text-lg">{product.rating}</span>
-                </div>
-              </div>
-            </div>
+            <Card key={product._id} product={product} />
           ))}
         </div>
       </div>
@@ -77,7 +44,6 @@ export async function getStaticProps() {
 function filterFeaturedProducts(allProducts) {
   const featuredProductsMap = new Map();
   const featuredProducts = [];
-
 
   allProducts.forEach((product) => {
     if (!featuredProductsMap.has(product.category)) {
